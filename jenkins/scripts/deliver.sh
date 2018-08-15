95b881c2b43eb383c32a03644cffc62c047d6a1e
jinl <hello_jinl@126.com>
2018-08-15 17:24:43 +0800

    修改了sh脚本

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -10,7 +10,7 @@ set +x
 
 echo 'copy scripts to target'
 set -x
-cp -R jenkins/* target
+cp -R jenkins/scripts/* target/scripts
 set +x
 
 echo 'The following complex command extracts the value of the <name/> element'
