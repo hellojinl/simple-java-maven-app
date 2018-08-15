3fbc01b1062b7a64f856e9e834184146d7010009
jinl <hello_jinl@126.com>
2018-08-15 16:10:52 +0800

    更改了代码

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "new version" );
+        System.out.println( "new version 1" );
+        System.out.println( "new version 2" );
     }
 }
