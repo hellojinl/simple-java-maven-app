6f2589a27550221793cdc9ad542471938f6674bc
jinl <hello_jinl@126.com>
2018-08-15 17:31:26 +0800

    修改了sh脚本

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -8,11 +8,6 @@ set -x
 mvn clean jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
-echo 'copy scripts to target'
-set -x
-cp -R jenkins/scripts/* target/scripts
-set +x
-
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
@@ -28,5 +23,9 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-pwd
 java -jar target/${NAME}-${VERSION}.jar
+
+echo 'copy scripts to target'
+set -x
+cp -R jenkins/scripts/ target/
+set +x
